55202b16017685de5f2d402398852b888241f3e1
Philip Meshach <philipmeshach.pm@gmail.com>
2022-09-05 23:47:28 +0530

    final commit

--- src/main/java/com/jap/sales/SalesDataAnalyzer.java
+++ src/main/java/com/jap/sales/SalesDataAnalyzer.java
@@ -7,13 +7,48 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class SalesDataAnalyzer {
-
-
     // This method reads a file and adds each line of the file into the corresponding SalesRecord object
+    SalesDataAnalyzer salesDataAnalyzer = null;
+    int countLines = 0;
     public SalesRecord[] readFile(String fileName) {
-        return null;
-    }
 
-
-
-}
+        try (
+                FileReader fileReader = new FileReader(fileName);
+                BufferedReader bufferedReader = new BufferedReader(fileReader)
+        ) {
+            String headers = bufferedReader.readLine();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                countLines++;
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        System.out.println("count - " + countLines);
+        SalesRecord[] salesRecord = new SalesRecord[countLines];
+        try (
+                FileReader fileReader = new FileReader(fileName);
+                BufferedReader bufferedReader = new BufferedReader(fileReader)
+        ) {
+            String line = bufferedReader.readLine();
+            int index = 0;
+            while ((line = bufferedReader.readLine()) != null) {
+                // 2016 Survey Response Q9a The following adequately support my work-related needs: City Manager�s Office 31.4 40.6 9.5
+                String[] dataInfo = line.split(",");
+                int customerId = Integer.parseInt(dataInfo[1]);
+                String date = dataInfo[0].trim();
+                int productCategory = Integer.parseInt(dataInfo[2]);
+                String paymentMethod = dataInfo[3].trim();
+                double amount = Double.parseDouble(dataInfo[4].trim());
+                double timeOnSite = Double.parseDouble(dataInfo[5].trim());
+                int clicksInSite = Integer.parseInt(dataInfo[6]);
+                SalesRecord newSalesRecord = new SalesRecord(date, customerId, productCategory,
+                        paymentMethod, amount, timeOnSite, clicksInSite);
+                salesRecord[index] = newSalesRecord;
+                index++;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return salesRecord;
+    }}
--- src/main/java/com/jap/sales/SalesRecord.java
+++ src/main/java/com/jap/sales/SalesRecord.java
@@ -26,6 +26,74 @@ public class SalesRecord {
 
     // Generate Getters and Setters
 
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public int getProduct_category() {
+        return product_category;
+    }
+
+    public void setProduct_category(int product_category) {
+        this.product_category = product_category;
+    }
+
+    public String getPayment_method() {
+        return payment_method;
+    }
+
+    public void setPayment_method(String payment_method) {
+        this.payment_method = payment_method;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getTime_on_site() {
+        return time_on_site;
+    }
+
+    public void setTime_on_site(double time_on_site) {
+        this.time_on_site = time_on_site;
+    }
+
+    public int getClicks_in_site() {
+        return clicks_in_site;
+    }
+
+    public void setClicks_in_site(int clicks_in_site) {
+        this.clicks_in_site = clicks_in_site;
+    }
+
     // Generate the toString method
 
+    @Override
+    public String toString() {
+        return "SalesRecord{" +
+                "date='" + date + '\'' +
+                ", customer_id=" + customer_id +
+                ", product_category=" + product_category +
+                ", payment_method='" + payment_method + '\'' +
+                ", amount=" + amount +
+                ", time_on_site=" + time_on_site +
+                ", clicks_in_site=" + clicks_in_site +
+                '}';
+    }
 }
