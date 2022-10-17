5802bc1e3bf6449a6219194b5f8f00bb5b059840
Philip Meshach <Philipmeshach.pm@gmail.com>
2022-10-17 09:41:34 +0530

    completed

--- src/main/java/com/jap/sales/SalesDataAnalyzer.java
+++ src/main/java/com/jap/sales/SalesDataAnalyzer.java
@@ -1,6 +1,7 @@
 package com.jap.sales;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
@@ -8,10 +9,9 @@ import java.text.SimpleDateFormat;
 
 public class SalesDataAnalyzer {
     // This method reads a file and adds each line of the file into the corresponding SalesRecord object
-    SalesDataAnalyzer salesDataAnalyzer = null;
+//   SalesDataAnalyzer salesDataAnalyzer = null;
     int countLines = 0;
     public SalesRecord[] readFile(String fileName) {
-
         try (
                 FileReader fileReader = new FileReader(fileName);
                 BufferedReader bufferedReader = new BufferedReader(fileReader)
@@ -35,8 +35,8 @@ public class SalesDataAnalyzer {
             while ((line = bufferedReader.readLine()) != null) {
                 // 2016 Survey Response Q9a The following adequately support my work-related needs: City Manager�s Office 31.4 40.6 9.5
                 String[] dataInfo = line.split(",");
-                int customerId = Integer.parseInt(dataInfo[1]);
                 String date = dataInfo[0].trim();
+                int customerId = Integer.parseInt(dataInfo[1]);
                 int productCategory = Integer.parseInt(dataInfo[2]);
                 String paymentMethod = dataInfo[3].trim();
                 double amount = Double.parseDouble(dataInfo[4].trim());
@@ -51,4 +51,5 @@ public class SalesDataAnalyzer {
             throw new RuntimeException(e);
         }
         return salesRecord;
-    }}
+    }
+}
